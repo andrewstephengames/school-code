8e54bc3bdf6dd6a398fb50f066e97c9605cf8e1a
andrewstephengames <andrewstephengames999@gmail.com>
2021-10-25 03:15:27 +0300

    Files left uncommitted.

---
+++ 20211023/3923.cpp
@@ -0,0 +1,78 @@
+//TODO: no output
+#include <iostream>
+using namespace std;
+int n, st[12],m;
+char v[26];
+
+void init (int k)
+{
+    st[k] = 0;
+}
+int succesor(int k)
+{
+    if(st[k]<n)
+     {
+         st[k]++;
+         return 1;
+     }
+     else
+        return 0;
+}
+int valid(int k)
+{
+    for (int i = 1; i < k; i++)
+        if (st[k] == st[i])
+            return 0;       
+    return 1;
+}
+int solutie(int k)
+{
+    return n == m;
+}
+void tipar()
+{
+    int i;
+    char c;
+    for(i=1;i<=m;i++)
+        cout << v[st[i]];
+    cout<<"\n";
+}
+void back(int k)
+{
+    int as,ev;
+//    st[1] = 0;
+//    init (k);
+    while(k>0)
+    {
+        do
+        {
+            as=succesor(k);
+            if (as) ev = valid(k);
+        }while(as && !ev);
+        if(as)
+          if(solutie(k))
+            tipar();
+          else
+            {
+                k++;
+                init(k);
+            }
+        else
+          k--;
+    }
+}
+int main()
+{
+    for (int i = 1; i <= 26; i++)
+    {
+        v[i] = i+64;
+    }
+    cin>>n>>m;
+//    tipar();
+    back(1);
+/*
+    for (int i = 1; i <= 26; i++)
+        cout << v[i] << " ";
+*/
+    return 0;
+}
