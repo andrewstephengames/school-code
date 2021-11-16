4e1d227feeecfb4597e0388666db7737c9da4427
andrewstephengames <andrewstephengames999@gmail.com>
2021-11-16 16:50:38 +0200

    20211116

---
+++ 20211116/back.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+int st[10], n;
+
+void init (int k)
+{
+    st[k] = 0;
+}
+
+int solutie (int k)
+{
+    return n == k;
+}
+
+int valid (int k)
+{
+    for (int i = 1; i < k; i++)
+        if (st[k] == st[i])
+            return 0;
+    return 1;
+}
+
+int succesor (int k)
+{
+    if (st[k] < n)
+    {
+        st[k]++;
+        return 1;
+    }
+    return 0;
+}
+
+void tipar ()
+{
+    for (int i = 1; i <= n; i++)
+        printf ("%d ", st[i]);
+    printf ("\n");
+}
+
+void back (int k)
+{
+    int as, ev;
+    while (k > 0)
+    {
+        init(k);
+        do
+        {
+            as = succesor(k);
+            if (as) ev = valid(k);
+        } while (as && !ev);
+        if (as)
+        {
+            if (solutie(k))
+                tipar();
+            else
+            {
+                k++;
+                init(k);
+            }
+        }
+        else k--;
+    }
+}
+
+int main ()
+{
+    scanf ("%d", &n);
+    back(1);
+    return 0;
+}
