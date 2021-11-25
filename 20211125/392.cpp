73a8bdbb904e1e641f48557e164c56f6fd931923
andrewstephengames <andrewstephengames999@gmail.com>
2021-11-25 14:55:31 +0200

    20211125

---
+++ 20211125/392.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int st[10];
+
+void init (int k)
+{
+    st[k] = -1;
+}
+
+void tipar ()
+{
+    for (int i = 1; i <= 5; i++)
+        cout << st[i];
+    cout << "\n";
+}
+
+int valid (int k)
+{
+    return 1;
+}
+
+int succesor (int k)
+{
+    if (st[k] < 1)
+    {
+        st[k]++;
+        return 1;
+    }
+    return 0;
+}
+
+int solutie (int k)
+{
+    return 5 == k;
+}
+
+void back (int k)
+{
+    int as, ev;
+    init(k);
+    while (k > 0)
+    {
+        do
+        {
+            as = succesor(k);
+            if (as) ev = valid(k);
+        } while (as && !ev);
+        if (as)
+        {
+            if (solutie(k))
+                tipar();
+            else
+            {
+                k++;
+                init(k);
+            }
+        }
+        else k--;
+    }
+}
+
+int main ()
+{
+    //cin >> n;
+    back(1);
+    cout << "\n" << (float)clock()/1000 << " ms\n";
+    return 0;
+}
+
