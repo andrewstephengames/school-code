bc21f7ac6ae429791087533c35d4a11317c280fc
andrewstephengames <andrewstephengames999@gmail.com>
2021-12-09 21:17:37 +0200

    20211209

---
+++ 20211209/divprim.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n, p, d = 2;
+    scanf ("%d", &n);
+    while (n > 1)
+    {
+        p = 0;
+        while (n%d == 0)
+        {
+            p++;
+            n /= d;
+        }
+        if (p)
+            if (p == 1)
+                printf ("%d\n", d);
+            else printf ("%d^%d\n", d, p);
+        d++;
+        if (n > 1 && d*d > n)
+            d = n;
+    }
+}
