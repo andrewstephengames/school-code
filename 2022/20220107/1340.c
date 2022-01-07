8d08f718f06b121e25e82926b1f54e40a5882619
andrewstephengames <andrewstephengames999@gmail.com>
2022-01-07 16:22:51 +0200

    1340

--- 2022/20220107/1340.c
+++ 2022/20220107/1340.c
@@ -1,63 +1,48 @@
-//TODO: segfault sort
+//TODO: 6936.00 for second test
 #include <stdio.h>
 
-struct Obiect
+struct obiect
 {
     int G, V;
-};
+} a[1001], tmp;
 
 int main ()
 {
-    int n, tmp1;
-    float eff[1001], castig = 0.0, gmax, tmp2;
-    struct Obiect a[2002];
+    int n;
+    float gmax, castig = 0.0, eff[1001];
     scanf ("%d %f", &n, &gmax);
     for (int i = 1; i <= n; i++)
     {
         scanf ("%d %d", &a[i].G, &a[i].V);
         eff[i] = a[i].V/a[i].G;
     }
-    /*
     for (int i = 1; i < n; i++)
-        for (int j = i+1; j <= n; i++)
-            if (eff[i] > eff[j])
+        for (int j = i+1; j <= n; j++)
+            if (eff[i] < eff[j])
             {
-                tmp1 = a[i].G;
-                a[i].G = a[j].G;
-                a[j].G = tmp1;
-                tmp1 = a[i].V;
-                a[i].V = a[j].V;
-                a[j].V = tmp1;
-                tmp2 = eff[i];
+                tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+                int tmp1 = eff[i];
                 eff[i] = eff[j];
-                eff[j] = tmp2;
+                eff[j] = tmp1;
             }
-    */
-    for (int i = 1; i <= n; i++)
-    {
-        int p = i;
-        while (p > 0 && eff[p] < eff[p-1])
-        {
-                tmp1 = a[p].G;
-                a[p].G = a[p-1].G;
-                a[p-1].G = tmp1;
-                tmp1 = a[p].V;
-                a[p].V = a[p-1].V;
-                a[p-1].V = tmp1;
-                tmp2 = eff[p];
-                eff[p] = eff[p-1];
-                eff[p-1] = tmp2;
-        }
-    }
-    int i = 1;
-    while (a[i].G <= gmax && i <= n)
+    int i;
+    for (i = 1; i <= n && a[i].G <= gmax; i++)
     {
         gmax -= a[i].G;
         castig += a[i].V;
-        if (gmax > 0 && i <= n)
-            castig += gmax*eff[i];
+    }
+    /*
+    while (a[i].G <= gmax && i <= n)
+    {
+        gmax -= a[i].G;
+        castig += gmax*eff[i];
         i++;
     }
-    printf ("%.0f\n", castig);
+    */
+    if (gmax > 0 && i <= n)
+        castig += gmax*eff[i];
+    printf ("%.2f ", castig);
     return 0;
 }
