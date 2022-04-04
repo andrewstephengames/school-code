5439c9f4e3f2c91fa6fa7744e68c2c1799c75217
andrewstephengames <andrewstephengames999@gmail.com>
2022-04-04 21:57:19 +0300

    20220404

---
+++ 2022/20220404/876.c
@@ -0,0 +1,41 @@
+//TODO: not grabbing last operation
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int n, c[1001], c_size;
+char op[10], *preplace;
+
+void push (int el)
+{
+     c_size++;
+     c[c_size] = el;
+}
+
+void pop ()
+{
+     c_size--;
+     for (int i = 1; i < c_size; i++)
+          c[i] = c[i+1];
+}
+
+void front ()
+{
+     printf ("%d\n", c[1]);
+}
+
+int main ()
+{
+     scanf ("%d", &n);
+     for (int i = 1; i <= n; i++)
+     {
+          fgets (op, 10, stdin);
+          if (op[0] == 'p' && op[1] == 'u')
+               push (atoi(op+5));
+          if (op[0] == 'f')
+               front ();
+          if (op[0] == 'p' && op[1] == 'o')
+               pop();
+     }
+     return 0;
+}
---
+++ 2022/20220404/teststring.c
@@ -0,0 +1,9 @@
+#include <string.h>
+#include <stdio.h>
+
+int main ()
+{
+     char stuff[] = "push 10";
+     printf ("%s\n", stuff+5);
+     return 0;
+}
